fae641892faf8a030c80ec3e586e824dfe55bf73
Lucca Larrossa <lucca.larrossa@gmail.com>
2023-09-15 16:38:07 -0300

    inicio barriga

---
+++ cypress/e2e/barriga/funcional.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe('Deve testar a nível funcional', () => {
+	beforeEach(() => {
+		cy.visit('barrigareact.wcaquino.me');
+		cy.reload();
+	})
+
+	it('Login', () => {
+
+	})
+})
